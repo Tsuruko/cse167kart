a7731a437704a9922749549d7c73a31eae33645a
Nick Troast <ntroast@Nicks-MacBook-Pro.local>
2013-11-18 14:25:54 -0800

    Added track width

--- Track.cpp
+++ Track.cpp
@@ -12,6 +12,7 @@ Track::Track()
 {
   curves.clear();
   stacks = 0.05;
+  width = 0.5;
 }
 
 void Track::addCurve(BCurve * c) {
@@ -49,6 +50,7 @@ void Track::drawTrack() {
     for (GLfloat j = 0; j <= 1+stacks; j += stacks) {
       Vector3 temp4((curves[i]->getTangent(j)));
         temp4.normalize();
+      temp4 = temp4.scale(width);
       glNormal3f(0, 0, -1);
 	glVertex3f(-temp4[1]+(curves[i]->getPoint(j))[0], 
 		   temp4[0]+(curves[i]->getPoint(j))[1], 
