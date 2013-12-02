e972eb1d400f0a8852dc5677be24579e87e355ef
Mona Liu <liu.mona.c@gmail.com>
2013-12-02 15:05:59 -0800

    added back up arrow functionailty

--- main.cpp
+++ main.cpp
@@ -186,8 +186,9 @@ void displayCallback(void)
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisable(GL_NORMAL_ARRAY);
     glEnable(GL_TEXTURE_2D);
-    cam.setEye(track->getNext(0.005, 0));
-    cam.setCenter(track->getNext(0.005, 1));
+
+    //cam.setEye(track->getNext(0.005, 0));
+    //cam.setCenter(track->getNext(0.005, 1));
   }
  
   glFlush();
@@ -225,6 +226,10 @@ void processKeys (unsigned char key, int x, int y) {
 
 void processSpecialKeys(int key, int x, int y) {
   switch(key) {
+    case GLUT_KEY_UP:
+      cam.setEye(track->getNext(0.015, 0));
+      cam.setCenter(track->getNext(0.015, 1));
+      break;
     case GLUT_KEY_LEFT:
       xtrans = xtrans - 0.2;
       if (xtrans < -1.8) xtrans = -1.8;
