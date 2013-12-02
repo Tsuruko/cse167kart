0831ea297e30e92fee997197899a7fe6bc0f86eb
Nick Troast <ntroast@Nicks-MacBook-Pro.local>
2013-12-02 15:24:58 -0800

    Fixed indentation 2 spaces

--- Track.cpp
+++ Track.cpp
@@ -38,8 +38,8 @@ void Track::drawCurves() {
   glBegin(GL_LINE_STRIP);
   for (int i = 0; i < curves.size(); i++) {
     for (GLfloat j = 0; j <= 1; j += stacks) {
-      glVertex3f(curves[i]->getPoint(j)[0], curves[i]->getPoint(j)[1], 
-        		curves[i]->getPoint(j)[2]);
+      glVertex3f(curves[i]->getPoint(j)[0], curves[i]->getPoint(j)[1],
+                 curves[i]->getPoint(j)[2]);
     }
   }
   glEnd();
@@ -62,85 +62,85 @@ void Track::drawPoints() {
 void Track::drawTrack() {
   glColor3f(1,1,1);
   glBegin(GL_QUAD_STRIP);
-  glEnable(GL_TEXTURE_2D); 
+  glEnable(GL_TEXTURE_2D);
   int texHeight = 0;
-  for (int i = 0; i < curves.size(); i++) { 
+  for (int i = 0; i < curves.size(); i++) {
     int vertCounter = 0;
     
     for (GLfloat j = 0; j <= 1; j += stacks) {
-//code for rendering track
+      //code for rendering track
       Vector3 temp4((curves[i]->getTangent(j)));
       temp4.normalize();
       temp4 = temp4.scale(width);
       texHeight++;
       if(texHeight>11) texHeight%=10;
- 
-     Vector3 v1(-temp4[1]+(curves[i]->getPoint(j))[0], 
-                 temp4[0]+(curves[i]->getPoint(j))[1], 
-        	 curves[i]->getPoint(j)[2]);
-
-      Vector3 v2(temp4[1]+(curves[i]->getPoint(j))[0], 
+      
+      Vector3 v1(-temp4[1]+(curves[i]->getPoint(j))[0],
+                 temp4[0]+(curves[i]->getPoint(j))[1],
+                 curves[i]->getPoint(j)[2]);
+      
+      Vector3 v2(temp4[1]+(curves[i]->getPoint(j))[0],
                  -temp4[0]+(curves[i]->getPoint(j))[1],
                  curves[i]->getPoint(j)[2]);
-
+      
       glTexCoord2f(0, (texHeight/10.0)); //Texture
       glNormal3f(0, 0, 1);
       glVertex3f(v1[0],v1[1],v1[2]);
-
+      
       glTexCoord2f(laneCount, (texHeight/10.0)); //Texture
       glNormal3f(0, 0, 1);
       glVertex3f(v2[0],v2[1],v2[2]);
-
-//code for rendering terrain
-         
-      Vector3 v3(-temp4[1]+(curves[i]->getPoint(j))[0], 
-            	 temp4[0]+(curves[i]->getPoint(j))[1], 
-        	 curves[i]->getPoint(j)[2]);
+      
+      //code for rendering terrain
+      
+      Vector3 v3(-temp4[1]+(curves[i]->getPoint(j))[0],
+                 temp4[0]+(curves[i]->getPoint(j))[1],
+                 curves[i]->getPoint(j)[2]);
       verticesOuter.push_back(v3);
-
+      
       if((vertCounter)%2==0){
         temp4 = temp4.scale(1.5);
-        Vector3 v4(-temp4[1]+(curves[i]->getPoint(j))[0], 
-        temp4[0]+(curves[i]->getPoint(j))[1], 
-        curves[i]->getPoint(j)[2]);
+        Vector3 v4(-temp4[1]+(curves[i]->getPoint(j))[0],
+                   temp4[0]+(curves[i]->getPoint(j))[1],
+                   curves[i]->getPoint(j)[2]);
         verticesInner.push_back(v4);
       }
       vertCounter++;
-  //verticesInner.push_back(genInner(verticesOuter[verticesOuter.size()-1],v1));
+      //verticesInner.push_back(genInner(verticesOuter[verticesOuter.size()-1],v1));
     }
-        glDisable(GL_TEXTURE_2D); 
+    glDisable(GL_TEXTURE_2D);
   }
   glEnd();
 }
 
 void Track::drawTerrain(){
-        /*
-  glColor3f(1,0,0);
-  glNormal3f(0, 0, 1);
-  glBegin(GL_LINE_STRIP);
-  for (int i = 0; i < verticesOuter.size(); i++) {
-      glVertex3f(verticesOuter[i][0],verticesOuter[i][1],verticesOuter[i][2]);
-  }
-  glEnd();
-
-  glColor3f(1,0,1);
-  glNormal3f(0, 0, 1);
-  glBegin(GL_LINE_STRIP);
-  for (int i = 0; i < verticesInner.size(); i++) {
-      glVertex3f(verticesInner[i][0],verticesInner[i][1],verticesInner[i][2]);
-  }
-  glEnd();*/
-
- glBegin(GL_TRIANGLE_STRIP);
+  /*
+   glColor3f(1,0,0);
+   glNormal3f(0, 0, 1);
+   glBegin(GL_LINE_STRIP);
+   for (int i = 0; i < verticesOuter.size(); i++) {
+   glVertex3f(verticesOuter[i][0],verticesOuter[i][1],verticesOuter[i][2]);
+   }
+   glEnd();
+   
+   glColor3f(1,0,1);
+   glNormal3f(0, 0, 1);
+   glBegin(GL_LINE_STRIP);
+   for (int i = 0; i < verticesInner.size(); i++) {
+   glVertex3f(verticesInner[i][0],verticesInner[i][1],verticesInner[i][2]);
+   }
+   glEnd();*/
+  
+  glBegin(GL_TRIANGLE_STRIP);
   glVertex3f(verticesOuter[0][0],verticesOuter[0][1],verticesOuter[0][2]);
   glVertex3f(verticesInner[0][0],verticesInner[0][1],verticesInner[0][2]);
-
+  
   for (int i = 1; i < verticesInner.size()/2; i++) {
-
-      glVertex3f(verticesOuter[2*i-1][0],verticesOuter[2*i-1][1],verticesOuter[2*i-1][2]);
-          glVertex3f(verticesInner[i][0],verticesInner[i][1],verticesInner[i][2]);
-          glVertex3f(verticesOuter[2*i][0],verticesOuter[2*i][1],verticesOuter[2*i][2]);
-
+    
+    glVertex3f(verticesOuter[2*i-1][0],verticesOuter[2*i-1][1],verticesOuter[2*i-1][2]);
+    glVertex3f(verticesInner[i][0],verticesInner[i][1],verticesInner[i][2]);
+    glVertex3f(verticesOuter[2*i][0],verticesOuter[2*i][1],verticesOuter[2*i][2]);
+    
   }
   glEnd();
   verticesOuter.clear();
@@ -150,9 +150,9 @@ void Track::drawTerrain(){
 Vector3 Track::getNext(GLfloat step, int test)
 {
   if (test == 0) {
-  //check if the tstep needs to be reset
+    //check if the tstep needs to be reset
     if (eye_t + step > 1) {
-    //loop back eyeCurve to first curve on the track
+      //loop back eyeCurve to first curve on the track
       if (eyeCurve == curves.size() - 1) eyeCurve = 0;
       else eyeCurve++;
       eye_t = step - (1 - eye_t);
@@ -173,7 +173,7 @@ Vector3 Track::getNext(GLfloat step, int test)
 
 
 Vector3 genInner(Vector3 v1, Vector3 v2){
-        Vector3 v3(0,0,0);
-        return v3;
-
+  Vector3 v3(0,0,0);
+  return v3;
+  
 }
