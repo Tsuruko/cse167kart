2b2dd4ceca28a44286daa72c56217ea7274156c1
Andy <rowlandking@gmail.com>
2013-12-07 01:44:21 -0800

    support for windows

--- geode.h
+++ geode.h
@@ -1,7 +1,12 @@
 #ifndef _GEODE_H_
 #define _GEODE_H_
 
+#ifdef __APPLE__
 #include <GLUT/glut.h>
+#else
+#include <cstdlib>
+#include <GL/glut.h>
+#endif
 #include <cstring>
 #include "Matrix4.h"
 
